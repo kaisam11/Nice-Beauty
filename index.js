a1f0d0155e48e4d19124ef0a0454c191387478be
kaisam11 <hrithiksinha17@gmail.com>
2021-04-18 14:39:11 +0530

    color update

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ const btn = document.querySelector('.nav-toggle')
 const header = document.querySelector('.header')
 
 btn.addEventListener('click', () => header.classList.toggle("open"))
+
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = header.getBoundingClientRect().height;
+
+    if (scrollHeight > navHeight) {
+        header.classList.add('down');
+    }
+
+    else {
+        header.classList.remove('down');
+    }
+})
