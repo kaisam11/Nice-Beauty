d6e6dfe6457d75cf5abd1741893e24f54ae3f8af
kaisam11 <hrithiksinha17@gmail.com>
2021-04-19 22:27:43 +0530

    Hopefully last

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+//preloader
 const preloader = document.querySelector('.preloader')
 
 window.addEventListener('load', () => {
     preloader.classList.add('hide-preloader');
 })
 
+//trasnition after loading
 const main = document.querySelector('.app')
-
 const banner = document.querySelector('.banner')
 
 window.onload = () => {
@@ -13,30 +14,34 @@ window.onload = () => {
     banner.classList.add('swipe-up')
 }
 
+//smooth scroll
 const r = rolly({
     view: document.querySelector('.app'),
     native: true,
 });
 r.init();
 
+//Animation on scroll
 AOS.init({
-    duration: 1500 //global duration
+    duration: 1500
 });
 
 const header = document.querySelector('.header')
 const btn = document.querySelector('.nav-toggle')
 
+//nav on mobile
 btn.addEventListener('click', () => header.classList.toggle("open"))
 
+//nav open
 window.addEventListener('scroll', () => {
-    const scrollHeight = window.pageYOffset;
+    const scrollHeight = window.scrollY;
     const navHeight = header.getBoundingClientRect().height;
 
     if (scrollHeight > navHeight) {
-        header.style.backgroundColor = "#000";
+        header.classList.add('fixed-header');
     }
 
     else {
-        header.style.backgroundColor = "transparent";
+        header.classList.remove('fixed-header');
     }
 })
